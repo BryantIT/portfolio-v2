8786934b59e3046e526373bc99ef4d8a7e233dd0
Bryant Richards <dragonseyeimages@gmail.com>
2021-04-15 12:04:10 -0500

    sidebar complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,39 +3,38 @@ import LineIcon from 'react-lineicons';
 import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
-    const [information, setInformation] = useState("");
-    const [navigationToggler, setNavigationToggler] = useState(false);
+  const [navigationToggler, setNavigationToggler] = useState(false)
 
-    const handleNavigationToggler = () => {
-        setNavigationToggler(!navigationToggler);
-    }
+  const handleNavigationToggler = () => {
+    setNavigationToggler(!navigationToggler)
+  }
 
 
-    return (
-        <nav className={navigationToggler ? "mi-header is-visible" : "mi-header"}>
-            <button onClick={handleNavigationToggler} className="mi-header-toggler">
-                {!navigationToggler ? <LineIcon name="menu" /> : <LineIcon name="close" />}
-            </button>
-            <div className="mi-header-inner">
-                <div className="mi-header-image">
-                    <Link to="/">
-                        <img src='/images/brand-image.jpg' alt="brandimage" />
-                    </Link>
-                </div>
-
-                <ul className="mi-header-menu">
-                    <li><NavLink exact to="/"><span>Home</span></NavLink></li>
-                    <li><NavLink to="/about"><span>About</span></NavLink></li>
-                    <li><NavLink to="/resume"><span>Resume</span></NavLink></li>
-                    <li><NavLink to="/portfolios"><span>Portfolios</span></NavLink></li>
-                    <li><NavLink to="/blogs"><span>Blogs</span></NavLink></li>
-                    <li><NavLink to="/contact"><span>Contact</span></NavLink></li>
-                </ul>
-                <p className="mi-header-copyright">&copy; {new Date().getFullYear()} <b><a rel="noopener noreferrer" target="_blank" href="https://nuclearthemes.com">NuclearThemes</a></b></p>
+  return (
+    <nav className={navigationToggler ? "mi-header is-visible" : "mi-header"}>
+        <button onClick={handleNavigationToggler} className="mi-header-toggler">
+            {!navigationToggler ? <LineIcon name="menu" /> : <LineIcon name="close" />}
+        </button>
+        <div className="mi-header-inner">
+            <div className="mi-header-image">
+                <Link to="/">
+                    <img src='/images/me-brand.png' alt="brandimage" />
+                </Link>
             </div>
-        </nav>
-    )
+
+            <ul className="mi-header-menu">
+                <li><NavLink exact to="/"><span>Home</span></NavLink></li>
+                <li><NavLink to="/about"><span>About</span></NavLink></li>
+                <li><NavLink to="/resume"><span>Resume</span></NavLink></li>
+                <li><NavLink to="/portfolios"><span>Portfolios</span></NavLink></li>
+                <li><NavLink to="/blogs"><span>Blog</span></NavLink></li>
+                <li><NavLink to="/contact"><span>Contact</span></NavLink></li>
+            </ul>
+            <p className="mi-header-copyright">&copy; {new Date().getFullYear()} <b><a rel="noopener noreferrer" target="_blank" href="https://nuclearthemes.com">The Code Ninja</a></b></p>
+        </div>
+    </nav>
+  )
 }
 
 
-export default Header;
+export default Header
