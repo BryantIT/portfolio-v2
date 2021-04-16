15d46438fdec1fe0c47d2807cc32bb3debbfef87
Bryant Richards <dragonseyeimages@gmail.com>
2021-04-15 20:42:26 -0500

    social links

--- src/components/Socialicons.js
+++ src/components/Socialicons.js
@@ -3,45 +3,26 @@ import axios from 'axios';
 import LineIcon from 'react-lineicons';
 
 const Socialicons = (props) => {
-  const [socialLinks, setSocialLinks] = useState({});
+  const [socialLinks, setSocialLinks] = useState({})
 
   useEffect(() => {
-    axios.get('/api/information')
-      .then(response =>{
-        setSocialLinks(response.data.socialLinks);
-      })
+    setSocialLinks({
+      linkedin: 'https://www.linkedin.com/in/bryant-richards/',
+      github: 'https://github.com/BryantIT',
+      twitter: 'https://twitter.com/mycodeninja'
+    })
   }, [])
 
   return (
     <ul className={props.bordered ? 'mi-socialicons mi-socialicons-bordered' : 'mi-socialicons'}>
-      {!socialLinks.facebook ? null : <li>
-        <a rel="noopener noreferrer" target="_blank" href={socialLinks.facebook}>
-          <LineIcon name="facebook"/>
-        </a>
-      </li>}
       {!socialLinks.twitter ? null : <li>
         <a rel="noopener noreferrer" target="_blank" href={socialLinks.twitter}>
           <LineIcon name="twitter"/>
         </a>
       </li>}
-      {!socialLinks.pinterest ? null : <li>
-        <a rel="noopener noreferrer" target="_blank" href={socialLinks.pinterest}>
-          <LineIcon name="pinterest"/>
-        </a>
-      </li>}
-      {!socialLinks.behance ? null : <li>
-        <a rel="noopener noreferrer" target="_blank" href={socialLinks.behance}>
-          <LineIcon name="behance"/>
-        </a>
-      </li>}
       {!socialLinks.linkedin ? null : <li>
         <a rel="noopener noreferrer" target="_blank" href={socialLinks.linkedin}>
           <LineIcon name="linkedin"/>
-        </a>
-      </li>}
-      {!socialLinks.dribbble ? null : <li>
-        <a rel="noopener noreferrer" target="_blank" href={socialLinks.dribbble}>
-          <LineIcon name="dribbble"/>
         </a>
       </li>}
       {!socialLinks.github ? null : <li>
@@ -50,7 +31,7 @@ const Socialicons = (props) => {
         </a>
       </li>}
     </ul>
-  );
+  )
 }
 
-export default Socialicons;
+export default Socialicons
